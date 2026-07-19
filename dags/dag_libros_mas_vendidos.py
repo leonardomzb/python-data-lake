837b8b1ac60bbace968e79c88399987161cda2bc
leonardomzb <leonardo.zamorano.b@gmail.com>
2026-07-19 14:27:49 -0400

    refactor: formatear con ruff y eliminar ejecucion asincrona

--- dags/dag_libros_mas_vendidos.py
+++ dags/dag_libros_mas_vendidos.py
@@ -1,34 +1,35 @@
 import os
-import asyncio
 from datetime import datetime, timedelta
 from pathlib import Path
 
 from airflow.decorators import dag, task
 
 from src.scripts.ingesta_bronze import ingesta_bronze
-from src.scripts.ingesta_silver import procesar_silver
 from src.scripts.ingesta_gold import generar_gold
+from src.scripts.ingesta_silver import procesar_silver
+
 
 RUTA_CONTENEDOR: Path = Path("/opt/airflow")
 
+
 @dag(
-    dag_id='pipeline_libros_buscalibre',
+    dag_id="pipeline_libros_buscalibre",
     default_args={
-        'owner': 'data_engineering_lab',
-        'depends_on_past': False,
-        'email_on_failure': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=3),
+        "owner": "data_engineering_lab",
+        "depends_on_past": False,
+        "email_on_failure": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=3),
     },
-    description='Pipeline Medallion (Bronze -> Silver) usando DuckDB y Polars',
-    schedule='@daily',
+    description="Pipeline Medallion (Bronze -> Silver) usando DuckDB y Polars",
+    schedule="@daily",
     start_date=datetime(2026, 7, 1),
     catchup=False,
-    tags=['laboratorio', 'polars', 'duckdb'],
+    tags=["laboratorio", "polars", "duckdb"],
 )
 def pipeline_libros_buscalibre():
 
-    @task(task_id='ingesta_capa_bronze')
+    @task(task_id="ingesta_capa_bronze")
     def run_ingesta_bronze(logical_date=None) -> str:
         # Cargando varibles de entorno
         url = os.getenv("BUSCALIBRE_URL")
@@ -36,52 +37,46 @@ def pipeline_libros_buscalibre():
         # Simulación de cabeceras de navegador (User-Agent) para evitar bloqueos
         headers = {"User-Agent": user_agent}
         # Obteniendo fecha logica para airflow
-        fecha_ejecucion = logical_date.strftime('%Y%m%d_%H%M%S')
-        
-        ruta_archivo_bronze = asyncio.run(ingesta_bronze(
-            url=url,
-            headers=headers,
-            fecha_logica=fecha_ejecucion,
-            raiz_proyecto=RUTA_CONTENEDOR
-        ))
+        fecha_ejecucion = logical_date.strftime("%Y%m%d_%H%M%S")
+
+        ruta_archivo_bronze = ingesta_bronze(
+            url=url, headers=headers, fecha_logica=fecha_ejecucion, raiz_proyecto=RUTA_CONTENEDOR
+        )
 
         return ruta_archivo_bronze
 
-    @task(task_id='procesar_capa_silver')
+    @task(task_id="procesar_capa_silver")
     def run_ingesta_silver(ruta_archivo_bronce: str, **context):
         if not ruta_archivo_bronce:
             raise ValueError("No se recibió ninguna ruta de archivo desde la capa Bronze.")
-        
-        logical_date = context['logical_date'] 
-        fecha_ejecucion = logical_date.strftime('%Y%m%d_%H%M%S')
+
+        logical_date = context["logical_date"]
+        fecha_ejecucion = logical_date.strftime("%Y%m%d_%H%M%S")
         print(f"Ruta recuperada nativamente para Silver: {ruta_archivo_bronce}")
 
         ruta_archivo_silver = procesar_silver(
-            fecha_logica=fecha_ejecucion,
-            raiz_proyecto=RUTA_CONTENEDOR,
-            bronce_file=ruta_archivo_bronce
+            fecha_logica=fecha_ejecucion, raiz_proyecto=RUTA_CONTENEDOR, bronce_file=ruta_archivo_bronce
         )
 
         return ruta_archivo_silver
-    
-    @task(task_id='generar_capa_gold')
+
+    @task(task_id="generar_capa_gold")
     def run_ingesta_gold(ruta_archivo_silver: str, **context):
         if not ruta_archivo_silver:
             raise ValueError("No se recibió ninguna ruta de archivo desde la capa Silver.")
-        
-        logical_date = context['logical_date'] 
-        fecha_ejecucion = logical_date.strftime('%Y%m%d_%H%M%S')
+
+        logical_date = context["logical_date"]
+        fecha_ejecucion = logical_date.strftime("%Y%m%d_%H%M%S")
         print(f"Ruta recuperada nativamente para Gold: {ruta_archivo_silver}")
 
         diccionario_rutas_gold = generar_gold(
-            fecha_logica=fecha_ejecucion,
-            raiz_proyecto=RUTA_CONTENEDOR,
-            silver_file=ruta_archivo_silver
+            fecha_logica=fecha_ejecucion, raiz_proyecto=RUTA_CONTENEDOR, silver_file=ruta_archivo_silver
         )
         return diccionario_rutas_gold
 
     ruta_bronce = run_ingesta_bronze()
     ruta_silver = run_ingesta_silver(ruta_bronce)
     run_ingesta_gold(ruta_silver)
+
 
 pipeline_libros_buscalibre()
--- src/scripts/ingesta_bronze.py
+++ src/scripts/ingesta_bronze.py
@@ -1,33 +1,32 @@
 import os
-import asyncio
 from datetime import datetime
 from pathlib import Path
-import httpx
-import pandas as pd
-from bs4 import BeautifulSoup
-import duckdb
 from typing import Any
+
+import duckdb
+import httpx
+import pyarrow as pa
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 
-async def ingesta_bronze(url: str, headers: dict[str, str], fecha_logica: str, raiz_proyecto: Path) -> str | None:
-    """ 
-    Extracción desde Buscalibre y almacenamiento
-    de los datos en formato Parquet dentro de la capa Bronze.  
+def ingesta_bronze(url: str, headers: dict[str, str], fecha_logica: str, raiz_proyecto: Path) -> str | None:
     """
-    transport = httpx.AsyncHTTPTransport(retries=3)
-    
-    async with httpx.AsyncClient(headers=headers, http2=True, transport=transport, timeout=12.0) as client:
+    Extracción desde Buscalibre y almacenamiento
+    de los datos en formato Parquet dentro de la capa Bronze.
+    """
+
+    with httpx.Client(headers=headers, http2=True, timeout=12.0) as client:
         try:
             print(f"Descargando datos desde: {url}")
-            response = await client.get(url)
+            response = client.get(url)
 
             response.raise_for_status()
-            
-            soup = BeautifulSoup(response.text, 'html.parser')
-            
-            bloques_libros = soup.find_all('div', class_='box-producto')
-            
+
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            bloques_libros = soup.find_all("div", class_="box-producto")
+
             if not bloques_libros:
                 raise ValueError("Error crítico: No se encontraron bloques de libros en el HTML.")
 
@@ -36,49 +35,58 @@ async def ingesta_bronze(url: str, headers: dict[str, str], fecha_logica: str, r
             records: list[dict[str, Any]] = []
 
             for posicion, bloque in enumerate(bloques_libros, start=1):
-                # Campos obligarotorios, de haber modificacion de diseño en BuscaLibre, se espera un error para actualizacion del script.
-                id_producto = bloque['data-id_producto']
-                titulo = bloque.find('h3', class_='nombre').text.strip()
-                autor = bloque.find('div', class_='autor').text.strip()
-                precio_final = bloque['data-precio']               
-                metadatos = bloque.find('div', class_='metas').text.strip()
-                
+                # Campos obligarotorios, de haber modificacion de diseño en BuscaLibre,
+                # se espera un error para actualizacion del script.
+                id_producto = bloque["data-id_producto"]
+                titulo = bloque.find("h3", class_="nombre").text.strip()
+                autor = bloque.find("div", class_="autor").text.strip()
+                precio_final = bloque["data-precio"]
+                metadatos = bloque.find("div", class_="metas").text.strip()
+
                 # Campos flexibles, se pueden almacenar como "None"
-                tag_desc = bloque.find('div', class_='descuento-v2')
+                tag_desc = bloque.find("div", class_="descuento-v2")
                 porcentaje_descuento = tag_desc.text.strip() if tag_desc else None
 
-                tag_del = bloque.find('del')
+                tag_del = bloque.find("del")
                 precio_antes = tag_del.text.strip() if tag_del else None
 
-                records.append({
-                    "ranking": posicion,
-                    "id_producto": id_producto,
-                    "titulo": titulo,
-                    "autor": autor,
-                    "metadatos": metadatos,
-                    "porcentaje_descuento": porcentaje_descuento,
-                    "precio_final": precio_final,
-                    "precio_antes": precio_antes,
-                    "fecha_ingesta": current_time.strftime('%Y-%m-%d')
-                })
+                records.append(
+                    {
+                        "ranking": posicion,
+                        "id_producto": id_producto,
+                        "titulo": titulo,
+                        "autor": autor,
+                        "metadatos": metadatos,
+                        "porcentaje_descuento": porcentaje_descuento,
+                        "precio_final": precio_final,
+                        "precio_antes": precio_antes,
+                        "fecha_ingesta": current_time.strftime("%Y-%m-%d"),
+                    }
+                )
 
-            df_bronce = pd.DataFrame(records)
             # Creacion de directorios con estándar Hive.
             lake_dir = raiz_proyecto / "lake" / "bronze" / "libros_mas_vendidos"
-            bronce_folder: Path = lake_dir / f"year={current_time:%Y}" / f"month={current_time:%m}" / f"day={current_time:%d}"
+            bronce_folder: Path = (
+                lake_dir / f"year={current_time:%Y}" / f"month={current_time:%m}" / f"day={current_time:%d}"
+            )
             bronce_folder.mkdir(parents=True, exist_ok=True)
 
             bronce_file_path: Path = bronce_folder / f"data_{fecha_logica}.parquet"
 
+            # records a tabla arrow
+            tabla_arrow = pa.Table.from_pylist(records)
+
             print("Guardando datos estructurados en la Capa Bronce...")
             # Conexión explícita y aislada en memoria para DuckDB
-            with duckdb.connect(database=':memory:') as con:
-                query = f"COPY df_bronce TO '{bronce_file_path.as_posix()}' (FORMAT 'PARQUET', COMPRESSION 'ZSTD')"
-                con.execute(query)        
+            with duckdb.connect(database=":memory:") as con:
+                con.from_arrow(tabla_arrow).write_parquet(
+                    bronce_file_path.as_posix(),
+                    compression="ZSTD"
+                )
 
             print(f"Éxito. Archivo estructurado guardado en Bronce: {bronce_file_path}")
             return bronce_file_path.as_posix()
-        
+
         except httpx.HTTPStatusError as http_err:
             print(f"Error de red o código de estado HTTP: {http_err}")
             raise
@@ -92,6 +100,7 @@ async def ingesta_bronze(url: str, headers: dict[str, str], fecha_logica: str, r
             print(f"Error crítico inesperado en la capa Bronce: {e}")
             raise
 
+
 if __name__ == "__main__":
     # Carga de variables de entorno
     load_dotenv()
@@ -101,14 +110,9 @@ if __name__ == "__main__":
     # Simulación de cabeceras de navegador (User-Agent) para evitar bloqueos
     HEADERS_LOCAL = {"User-Agent": USER_AGENT_LOCAL}
 
-    #Valores default solo para pruebas de script individuales
+    # Valores default solo para pruebas de script individuales
     DEFAULT_ROOT: Path = Path(__file__).resolve().parents[2]
     FECHA_DEFAULT: str = datetime.now().strftime("%Y%m%d_%H%M%S")
 
     print("Prueba local bronze...")
-    asyncio.run(ingesta_bronze(
-        url=URL_LOCAL,
-        headers=HEADERS_LOCAL,
-        fecha_logica=FECHA_DEFAULT,
-        raiz_proyecto=DEFAULT_ROOT
-    ))
+    ingesta_bronze(url=URL_LOCAL, headers=HEADERS_LOCAL, fecha_logica=FECHA_DEFAULT, raiz_proyecto=DEFAULT_ROOT)
